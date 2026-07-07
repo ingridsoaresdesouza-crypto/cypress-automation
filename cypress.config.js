20b17a25d242dfe49cb65ebdcb035f3a88ae3438
ingrid osorio <ingrid.osorio@senno.ai>
2026-07-08 01:05:38 +0300

    Add Mochawesome test report configuration

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,21 @@ module.exports = defineConfig({
   projectId: 'bw6vzs',
   allowCypressEnv: false,
 
-  
+  reporter: 'cypress-mochawesome-reporter',
+
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
     baseUrl: 'https://automationpratice.com.br/',
+
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      // implement node event listeners here
+      return config;
     },
   },
 });
